44beb05c364146a0536279ce41faffdce81f46be
thekinv21 <vadimkiniabaev@gmail.com>
2022-12-02 14:56:45 +0300

    add profile page

---
+++ src/Screens/Profile/Profile.jsx
@@ -0,0 +1,13 @@
+import React, { Fragment } from "react";
+import Navbar from "../../Components/Navbar/Navbar";
+
+
+const Profile = () => {
+  return (
+    <Fragment>
+        <Navbar/>
+    </Fragment>
+  );
+};
+
+export default Profile;
