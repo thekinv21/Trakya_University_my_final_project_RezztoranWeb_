da9f49101fd62117f8c4389530b4872d24228bb3
thekinv21 <vadimkiniabaev@gmail.com>
2022-12-08 23:01:04 +0300

    add any Components to Landing Page

--- src/Screens/Landing/Landing.jsx
+++ src/Screens/Landing/Landing.jsx
@@ -1,28 +1,32 @@
-import React from "react";
+import React, { Fragment } from "react";
 import { Stack, Box } from "@chakra-ui/react";
 import Header from "../../Components/Header/Header";
 import Search from "../../Components/Search/Search";
 import Background from "../../Assets/images/Background.png";
 import RestoransList from "../../Components/RestoransList/RestoransList";
+import AboutUs from "../../Components/AboutUs/AboutUs";
 import Footer from "../../Common/Footer/Footer";
 const Landing = () => {
   return (
-    <Box
-      maxW="100vw"
-      h="1200px"
-      maxH={"1200px"}
-      bgColor="#B2BEB5"
-      bgImage={Background}
-    >
-      <Header />
+    <Fragment>
+      <Box
+        maxW="100vw"
+        minH="1400px"
+        maxH="auto"
+        bgColor="#B2BEB5"
+        bgImage={Background}
+        overflowY="hidden"
+      >
+        <Header />
 
-      <Stack spacing={20} direction="column" pt="10rem">
-        <Search />
-        <RestoransList />
-      </Stack>
-
-      <Footer />
-    </Box>
+        <Stack spacing={20} direction="column" pt="8rem" h="auto">
+          <Search />
+          <RestoransList />
+          <AboutUs />
+          <Footer />
+        </Stack>
+      </Box>
+    </Fragment>
   );
 };
 
