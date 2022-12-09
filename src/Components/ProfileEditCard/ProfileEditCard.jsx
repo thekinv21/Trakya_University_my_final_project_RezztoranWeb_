7663bfe14f0e1af35b79d6df03238b57da961ecb
thekinv21 <vadimkiniabaev@gmail.com>
2022-12-09 11:26:55 +0300

    update profile edit card

--- src/Components/ProfileEditCard/ProfileEditCard.jsx
+++ src/Components/ProfileEditCard/ProfileEditCard.jsx
@@ -14,7 +14,7 @@ import { MdAddAPhoto } from "react-icons/md";
 
 const ProfileEditCard = () => {
   return (
-    <Stack direction="column" spacing="8">
+    <Stack direction="column" spacing="6" mt="30px">
       {/*-----------------------Card-1 Title------------------------- */}
 
       <Stack>
@@ -32,10 +32,9 @@ const ProfileEditCard = () => {
           bg="#fff"
           color="#000"
           boxShadow="rgb(51 51 51 / 10%) 0px 1px 4px"
-          minW={["320px", "450px", "440px", "570px"]}
+          minW={["425px", "495px", "440px", "570px"]}
           maxW={["340px", "340px", "340px", "570px"]}
           h={["500px", "600px", "500px", "500px"]}
-          borderRadius="10px"
           p="16px"
           display="flex"
           flexDirection="column"
@@ -162,10 +161,9 @@ const ProfileEditCard = () => {
         <Card
           bg="#fff"
           boxShadow="rgb(51 51 51 / 10%) 0px 1px 4px"
-          minW={["320px", "450px", "440px", "570px"]}
+          minW={["425px", "495px", "440px", "570px"]}
           maxW={["340px", "100%", "340px", "570px"]}
           h={["100px", "100px", "100px", "100px"]}
-          borderRadius={["none", "10px", "10px"]}
           textAlign="center"
           display="flex"
           justifyContent="center"
