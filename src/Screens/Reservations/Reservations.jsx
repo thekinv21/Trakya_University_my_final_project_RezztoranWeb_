2231a5a4fc2052ccd04ab2baa20ee32523ae5546
thekinv21 <vadimkiniabaev@gmail.com>
2022-12-10 16:10:46 +0300

    create Reservations page

---
+++ src/Screens/Reservations/Reservations.jsx
@@ -0,0 +1,49 @@
+import React, { Fragment } from "react";
+import Navbar from "../../Common/Navbar/Navbar";
+import { Stack, Box } from "@chakra-ui/react";
+import Background from "../../Assets/images/Background.png";
+import Footer from "../../Common/Footer/Footer";
+import ProfileCard from "./../../Components/ProfileCard/ProfileCard";
+import ReservationCard from "../../Components/ReservationCard/ReservationCard";
+
+const Reservations = () => {
+  return (
+    <Fragment>
+      <Navbar />
+
+      <Box
+        bgImage={Background}
+        bgColor="#B2BEB5"
+        maxW="100vw"
+        minH="100vh"
+        maxH="auto"
+        p="8% 20%"
+        position="relative"
+        zIndex="0"
+      >
+        <Stack
+          w="100%"
+          h="100%"
+          spacing={12}
+          direction={["column", "column", "column", "row"]}
+          alignItems={["center", "center", "center", "center"]}
+          justifyContent={["center", "center", "center", "start"]}
+        >
+          {/*------------------------Profile Card Component-------------------------- */}
+          <Box display={["none", "none", "none", "block"]}>
+            <ProfileCard />
+          </Box>
+
+          {/*------------------------Reservations İnfo Card-------------------------- */}
+          <Box>
+            <ReservationCard />
+          </Box>
+        </Stack>
+      </Box>
+
+      <Footer />
+    </Fragment>
+  );
+};
+
+export default Reservations;
