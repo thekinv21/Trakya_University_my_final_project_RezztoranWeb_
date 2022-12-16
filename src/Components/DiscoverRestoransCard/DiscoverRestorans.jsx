3cce8de4d88322a1ffb06d8555bb3911e86073fb
thekinv21 <vadimkiniabaev@gmail.com>
2022-12-17 02:18:25 +0300

    update restoransCard

--- src/Components/DiscoverRestoransCard/DiscoverRestorans.jsx
+++ src/Components/DiscoverRestoransCard/DiscoverRestorans.jsx
@@ -7,15 +7,13 @@ import {
   Image,
   Text,
   Code,
-  CardFooter,
+  Link,
   Button,
 } from "@chakra-ui/react";
 import axios from "axios";
-import { useNavigate } from "react-router-dom";
 
 const DiscoverRestorans = () => {
   const [discoverRestorants, setDiscover] = useState([]);
-  const navigate = useNavigate();
 
   useEffect(() => {
     async function getRestaurants() {
@@ -34,82 +32,64 @@ const DiscoverRestorans = () => {
       {discoverRestorants.map((restaurant) => (
         <Card
           direction={{ base: "column", sm: "row" }}
+          overflow="hidden"
           bg="#fff"
           key={restaurant.id}
         >
           <Image
             objectFit="cover"
-            w={["330px", "250px", "200px", "250px"]}
+            maxW={{ base: "100%", sm: "200px" }}
             src={restaurant.imgURL}
             alt={restaurant.title}
           />
 
-          <Stack>
+          <Stack
+            direction={"row"}
+            w="100%"
+            flexDirection={["column", "column", "column", "row"]}
+            justifyContent={["start", "start", "start", "space-between"]}
+          >
             <CardBody>
-              <Heading size="md">{restaurant.title}</Heading>
+              <Heading fontFamily={"unset"} fontSize="20px">
+                {restaurant.title}
+              </Heading>
 
-              <Text fontSize="small">
-                <Code
-                  children="City:"
-                  mr="10px"
-                  colorScheme={"orange"}
-                  mt="10px"
-                />
+              <Text pt="10px" fontSize={14}>
                 {restaurant.city}
               </Text>
 
-              <Text fontSize="small">
-                <Code
-                  children="District:"
-                  mr="10px"
-                  colorScheme={"orange"}
-                  mt="10px"
-                />
+              <Text pt="5px" fontSize={12}>
                 {restaurant.district}
               </Text>
 
-              <Text py="2" fontSize="small">
-                <Code children="Address:" mr="10px" colorScheme={"orange"} />
-                {restaurant.detailedAddress}
-              </Text>
-
-              <Text fontSize="small">
-                <Code children="Category:" mr="10px" colorScheme={"orange"} />
-                {restaurant.category}
-              </Text>
-
-              <Text fontSize="small">
-                <Code
-                  children="Most Rated:"
-                  mr="10px"
-                  colorScheme={"orange"}
-                  mt="10px"
-                />
+              <Text pt="5px" fontSize={12}>
                 {restaurant.most_rated}
               </Text>
 
-              <Text fontSize="small">
-                <Code
-                  children="Star:"
-                  mr="10px"
-                  colorScheme={"orange"}
-                  mt="10px"
-                />
-                {restaurant.star}
-              </Text>
+              <Code
+                children="Aile İçin Uygundur"
+                mt="10px"
+                fontSize={10}
+                borderRadius={5}
+              />
             </CardBody>
 
-            <CardFooter p="0px 0px 20px 15px" alignItems="start">
+            <CardBody
+              display="flex"
+              alignItems="center"
+              justifyContent={["center", "center", "center", "flex-end"]}
+            >
               <Button
-                colorScheme="green"
+                w={["100%", "100%", "120px", "120px"]}
+                href="#"
+                as={Link}
+                colorScheme="whatsapp"
                 fontSize="small"
-                fontWeight="regular"
-                w="130px"
-                onClick={() => (navigate('#'))}
+                fontWeight={200}
               >
                 Masa Ayirt
               </Button>
-            </CardFooter>
+            </CardBody>
           </Stack>
         </Card>
       ))}
