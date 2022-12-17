21f8299ecf81e8dce1a5b1d47560860c9743ceed
thekinv21 <vadimkiniabaev@gmail.com>
2022-12-18 00:48:20 +0300

    change profile card bg

---
+++ src/Components/Profile_Card/ProfileCard.jsx
@@ -0,0 +1,67 @@
+import React from "react";
+import {
+  Card,
+  Flex,
+  Text,
+  Image,
+  CardBody,
+  Link,
+  UnorderedList,
+  ListItem,
+} from "@chakra-ui/react";
+import UserSvg from "../../Assets/Svg/ProfileCardUser.svg";
+import { ProfileMenuLinks } from "../Links/Links";
+
+const ProfileCard = () => {
+  return (
+    <Card
+      bg="#fff"
+      minW="280px"
+      maxW="300px"
+      h="500px"
+      p="30px 0px"
+    >
+      <Flex
+        w="100%"
+        h="100%"
+        alignItems="center"
+        flexDirection="column"
+        justifyContent="center"
+      >
+        <Image src={UserSvg} />
+
+        {/*---------------User giriş yaptiğinda Nicki gözükecek Unutmaa-------------- */}
+
+        <Text pt="30px" fontSize="14px">
+          Tekrar hoş geldiniz, User..
+        </Text>
+      </Flex>
+
+      {/*------------------------------------Menu Links-------------------------------- */}
+
+      <CardBody p="0px" mt="20px">
+        {ProfileMenuLinks.map((cardLink, index) => (
+          <UnorderedList key={index} listStyleType="none">
+            <Link
+              href={cardLink.href}
+              borderTop="0.5px solid #c1c1c1"
+              display="flex"
+              alignItems="center"
+              p="12px 4px"
+              fontSize="14px"
+              _hover={{
+                borderLeft: "8px solid rgb(248, 179, 51)",
+                color: "#FAC050",
+              }}
+            >
+              <ListItem pr="10px"> {cardLink.icon}</ListItem>
+              <ListItem fontWeight="300">{cardLink.name}</ListItem>
+            </Link>
+          </UnorderedList>
+        ))}
+      </CardBody>
+    </Card>
+  );
+};
+
+export default ProfileCard;
