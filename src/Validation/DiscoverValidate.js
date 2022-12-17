474fc2cd56126dc56132d6cf651fef8f0cd57261
thekinv21 <vadimkiniabaev@gmail.com>
2022-12-18 00:44:38 +0300

    update formik validations

--- src/Validation/DiscoverValidate.js
+++ src/Validation/DiscoverValidate.js
@@ -4,9 +4,6 @@ let restaurantFilter = object({
   restaurantTitle: string().required(),
   restaurantPrice: number().required(),
   restaurantStars: number().min(1).max(5).required(),
-  persons: number().min(1).required(),
-  restaurantEmptyDate: string().required(),
-  restaurantEmptyTime: string().required()
 });
 
 export default restaurantFilter;
